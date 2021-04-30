f739a8cc56ff30d8e93a090e38283a1911a3a6cb
Ali Spittel <ali07cat07@gmail.com>
2021-04-30 14:35:21 -0500

    app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
+import config from '../src/aws-exports'
+import { Amplify } from 'aws-amplify'
 
+Amplify.configure({ ...config, ssr: true })
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
